f249ddc46c0202ad508fec6d3ad525e4985ece78
YCaTGameRT <ycatgamert@gmail.com>
2026-05-18 05:16:45 +0500

    created project and basic class

---
+++ TasksList.cpp
@@ -0,0 +1,3 @@
+#include "TasksList.h"
+
+TasksList::TasksList() {}
---
+++ TasksList.h
@@ -0,0 +1,11 @@
+#ifndef _TASKSLIST_H_
+#define _TASKSLIST_H_
+
+class TasksList {
+public:
+    TasksList();
+private:
+
+};
+
+#endif
