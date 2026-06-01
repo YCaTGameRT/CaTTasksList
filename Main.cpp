36354c26a0fe2af5a975a592969c5522932d2e82
YCaTGameRT <ycatgamert@gmail.com>
2026-06-01 23:40:16 +0500

    rus win

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,19 @@
+#ifdef __linux__
 #include <locale.h>
+#elif _WIN32
+#include <Windows.h>
+#endif
 
 #include "Executable.h"
 
 int main() {
+    #ifdef __linux__
     setlocale(LC_ALL, "Russian");
+    #elif _WIN32
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+    #endif
+    
 
     Executable exe;
     exe.Run();
